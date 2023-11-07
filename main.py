658bbde17494f4df8a28daeb8d22a07a5321f235
Kate <z.katusha5@gmail.com>
2023-11-07 22:13:55 +0300

    добавлены коментарии

--- main.py
+++ main.py
@@ -1,52 +1,25 @@
-#2. Дана действительная матрица размером n х m, все элементы которой различны.
-#   В каждой строке выбирается элемент с наименьшим значением.
-#   Если число четное, то заменяется нулем, нечетное - единицей. Вывести на экран новую матрицу.
+#5.	Реализовать модуль, содержащий функцию нахождения в массиве целых чисел
+#   разности индексов максимального и минимального элементов.
 
-print('Enter size of matrix: ')
-n = input('Enter n: ')
-while True:
+import find_difference
+
+n = input("Enter the size of the array: ")
+while True: #проверка числа n
     try:
         n = int(n)
         break
     except:
-        n = input('Enter correct n:  ')
-m = input('Enter m: ')
-while True:
-    try:
-        m = int(m)
-        break
-    except:
-        m = input('Enter correct m:  ')
+        n = input('Enter correct size of the array:  ')
 
-mas = []
-i = 0
-j = 0
+arr = []
 for i in range(n):
-    row = []
-    print('Enter new row')
-    for j in range(m):
-        while 1:
-            x = input('Enter element: ')
-            while True:
-                try:
-                    x = float(x)
-                    break
-                except:
-                    x = input('Enter correct element:  ')
-            if any(x in r for r in mas):
-                print("The value is already there, enter another one: ")
-            else:
-                break
-        row.append(x)
-    mas.append(row)
-print(mas)
+    x = input('Enter element: ')
+    while True: #проверка каждого нового введенного элемента
+        try:
+            x = int(x)
+            break
+        except:
+            x = input('Enter correct previous element:  ')
+    arr.append(x)
 
-mas2 = mas
-for i in range(n):
-    min_el = min(mas2[i])
-    for idx, val in enumerate(mas2[i]):
-#Функция enumerate() применяется для прохода по значениям коллекции,
-#при этом предоставляется информация о номере каждого элемента.
-        if val == min_el and int(min_el) == min_el:
-            mas2[i][idx] = min_el % 2
-print(mas2)
+print(find_difference.difference(arr))
